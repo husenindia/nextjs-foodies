272d6fe2195cf0ec6e72d9e8c0e1932d0bfb0270
husen.telwala <husentelwala@gmail.com>
2026-06-12 21:33:43 +0530

    meal database service

--- lib/meals.tsx
+++ lib/meals.tsx
@@ -1,8 +1,13 @@
 import sql from 'better-sqlite3';
-
 const db = new sql('meals.db');
 
 export async function getMeals() {
-    await new Promise((resolve)=> setTimeout(resolve, 5000));
+    await new Promise((resolve)=> setTimeout(resolve, 1000));
+//    throw new Error("Loading meals failed");
     return await db.prepare('SELECT * FROM meals').all();
+}
+
+
+export function getMeal(slug) {
+    return db.prepare('SELECT * FROM meals where slug = ?').get(slug);
 }
